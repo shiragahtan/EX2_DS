98cff274cbb06b2ea7b09ba5b1807a6d36acfae3
shiragahtan <shiragahtan2@gmail.com>
2023-06-02 15:08:05 +0300

    shira changes

---
+++ Classes.cpp
@@ -0,0 +1,10 @@
+//
+// Created by shira on 02/06/2023.
+//
+
+#include "Classes.h"
+
+Costumer::Costumer(int c_id, int phone) : m_c_id(c_id), m_phoneNum(phone), clubMember(false) {};
+
+Record::Record(int r_id, int numCopies, int numPurchases) : m_r_id(r_id), m_numCopies(numCopies), m_numPurchases(numPurchases) {};
+
--- HashTable.h
+++ HashTable.h
@@ -1,10 +1,13 @@
 //
 // Created by shira on 01/06/2023.
 //
-#include "AvlTree.h"
-#include "recordsCompany.h"
 #ifndef EX2_DS_STRUCTURES_H
 #define EX2_DS_STRUCTURES_H
+
+#include "Classes.h"
+#include "AvlTree.h"
+
+//class HashTable;
 
 class HashTable{
     int m_size;
@@ -25,21 +28,5 @@ public:
 
 static int START_SIZE = 2;
 static int EXPAND_RATE = 2;
-
-int main(){
-    HashTable hash1;
-    Costumer costumer1(1,1);
-    Costumer costumer2(2,1);
-    Costumer costumer3(3,1);
-    Costumer costumer4(4,1);
-    Costumer costumer9(9,1);
-
-    hash1.insertCostumer(costumer1);
-    hash1.insertCostumer(costumer2);
-    Costumer searching = hash1.search(2);
-    hash1.insertCostumer(costumer3);
-    hash1.insertCostumer(costumer4);
-    hash1.insertCostumer(costumer9);
-}
 
 #endif //EX2_DS_STRUCTURES_H
