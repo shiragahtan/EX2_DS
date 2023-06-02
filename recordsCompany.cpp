aeea0d3f22b11667708baa76cf0b0736e6ec30ac
Omer Oz <omer.oz@campus.technion.ac.il>
2023-06-02 17:25:35 +0300

    "Omer on UF"

--- recordsCompany.cpp
+++ recordsCompany.cpp
@@ -10,48 +10,21 @@ StatusType RecordsCompany::addCostumer(int c_id, int phone) {
     if (c_id < 0 || phone < 0){
         return INVALID_INPUT;
     }
-    if (membersHash.searchIfExists(c_id) != DOESNT_EXIST){
+    if (membersHash.searchIfExists(c_id) == DOESNT_EXIST){
         return ALREADY_EXISTS;
     }
-    Costumer newCostumer(c_id, phone);
-    if (membersHash.insertCostumer(newCostumer) == ALLOCATION_ERROR){
-        return ALLOCATION_ERROR;
-    }
-    return SUCCESS;
-}
 
-Output_t<int> RecordsCompany::getPhone(int c_id){
-    if (c_id < 0){
-        return INVALID_INPUT;
-    }
-    int searchAnswer = membersHash.searchIfExists(c_id);
-    if (searchAnswer == DOESNT_EXIST){
-        return Output_t<int>(DOESNT_EXIST);
-    }
-    return Output_t<int>(searchAnswer);
 }
-
-RecordsCompany::~RecordsCompany() {};
 
 StatusType RecordsCompany::newMonth(int *records_stocks, int number_of_records) {
     auto UFArray= new UFNode*[number_of_records];
     for(int i=0;i<number_of_records;i++){
-        UFArray[i]=new UFNode(i);
-    }
-    UF=new UnionFind(UFArray);
-    //TODO: to update about money of members in AVLTREE
+        UFArray[i]=new UFNode(i,records_stocks[i]);
+        columnsArr[i]=i;
 }
+UF=new UnionFind(UFArray);
+recordNum=number_of_records;
 
-Output_t<bool> RecordsCompany::isMember(int c_id){
-    if (c_id < 0){
-        return INVALID_INPUT;
-    }
-    int searchAnswer = membersHash.searchIfExists(c_id);
-    if (searchAnswer == NOT_IN_HASH){
-        return Output_t<bool>(DOESNT_EXISTS);
-    }
-    Costumer costumer = membersHash.search(c_id);
-    return Output_t<bool>(costumer.clubMember);
 }
 
 StatusType RecordsCompany::putOnTop(int r_id1, int r_id2) {
@@ -67,19 +40,4 @@ StatusType RecordsCompany::putOnTop(int r_id1, int r_id2) {
     }
     int root= UF->find(r_id1);
     columnsArr[root]=columnsArr[r_id2];
-}
-
-StatusType RecordsCompany::makeMember(int c_id) {
-    if (c_id < 0){
-        return INVALID_INPUT;
-    }
-    int searchAnswer = membersHash.searchIfExists(c_id);
-    if (searchAnswer == NOT_IN_HASH){
-        return DOESNT_EXISTS;
-    }
-    Costumer costumerToUpdate = membersHash.search(c_id);
-    if (costumerToUpdate.clubMember){
-        return ALREADY_EXISTS;
-    }
-    //TODO: add the member to the tree
 }
