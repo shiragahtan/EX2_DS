2971e4540df34abf4c99e0fd6880c4633d02093b
Omer Oz <omer.oz@campus.technion.ac.il>
2023-06-02 15:23:53 +0300

    "Omer on UF"

--- recordsCompany.cpp
+++ recordsCompany.cpp
@@ -22,7 +22,6 @@ Costumer::Costumer(int c_id, int phone) : m_c_id(c_id), m_phoneNum(phone) {};
 
 Record::Record(int r_id, int numCopies, int numPurchases) : m_r_id(r_id), m_numCopies(numCopies), m_numPurchases(numPurchases) {};
 
-RecordsCompany::RecordsCompany() {};
 RecordsCompany::~RecordsCompany() {};
 
 StatusType RecordsCompany::newMonth(int *records_stocks, int number_of_records) {
