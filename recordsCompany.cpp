b84ca10fc448ade5565c42a251f7a683c8d758b4
Omer Oz <omer.oz@campus.technion.ac.il>
2023-06-06 19:41:19 +0300

    "Omer on Testing"

--- recordsCompany.cpp
+++ recordsCompany.cpp
@@ -8,8 +8,8 @@ RecordsCompany::RecordsCompany() : membersTree() , membersHash(),numberOfPurchas
 
 
 RecordsCompany:: ~RecordsCompany(){ //update the dctor
-    membersHash.~HashTable();
-    membersTree.~AvlTree();
+    //membersHash.~HashTable();
+    //membersTree.~AvlTree();
     if (isNewMonth){
         delete[] columnsArr;
         delete[] numberOfPurchases;
